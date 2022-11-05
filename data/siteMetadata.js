670ad4baec79f7937e4a5dd5ecbda009346d98dc
Nicolas Kheirallah <Nicolas.kheirallah@gmail.com>
2022-11-05 17:09:33 +0100

    small fix

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -8,7 +8,7 @@ const siteMetadata = {
   siteUrl: 'https://yourmodernworkplace.com/',
   siteRepo: 'https://github.com/NicolasKheirallah/NextJs-M365-Blog',
   siteLogo: '/static/images/logo.png',
-  image: '/static/images/52460329_10216116934708393_7022385805154844672_n (1).jpg',
+  image: '/static/images/avatar.jpg',
   socialBanner: '/static/images/twitter-card.png',
   email: 'Nicolas.kheirallah@gmail.com',
   github: 'https://github.com',
