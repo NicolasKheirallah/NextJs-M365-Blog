deba8809582c33b2677376ae965ead657030f0a8
Nicolas Kheirallah <Nicolas.kheirallah@gmail.com>
2022-11-14 17:34:35 +0100

    Trying ms clarity

--- pages/_app.js
+++ pages/_app.js
@@ -14,7 +14,30 @@ import { ClientReload } from '@/components/ClientReload'
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
 
+function Page() {
+  return (
+    <>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+         (function(c,l,a,r,i,t,y){
+             c[a] = c[a] || function () { (c[a].q = c[a].q || 
+             []).push(arguments) };
+             t=l.createElement(r);
+             t.async=1;
+             t.src="https://www.clarity.ms/tag/"+i;
+             y=l.getElementsByTagName(r)[0];
+             y.parentNode.insertBefore(t,y);
+         })(window, document, "clarity", "script", "eiw0lj3ita");`,
+        }}
+      />
+      ;
+    </>
+  )
+}
+
 export default function App({ Component, pageProps }) {
+  this.Page()
   return (
     <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
       <Head>
@@ -28,17 +51,3 @@ export default function App({ Component, pageProps }) {
     </ThemeProvider>
   )
 }
-;<script
-  dangerouslySetInnerHTML={{
-    __html: `
-         (function(c,l,a,r,i,t,y){
-             c[a] = c[a] || function () { (c[a].q = c[a].q || 
-             []).push(arguments) };
-             t=l.createElement(r);
-             t.async=1;
-             t.src="https://www.clarity.ms/tag/"+i;
-             y=l.getElementsByTagName(r)[0];
-             y.parentNode.insertBefore(t,y);
-         })(window, document, "clarity", "script", "eiw0lj3ita");`,
-  }}
-/>
