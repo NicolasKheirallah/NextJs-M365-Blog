f088e69c6f87fafe1c4b0df694da5f42400eaae0
Nicolas Kheirallah <Nicolas.kheirallah@gmail.com>
2022-11-14 17:29:59 +0100

    testing ms Clarity

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,6 @@ import '@fontsource/inter'
 
 import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
-
 import siteMetadata from '@/data/siteMetadata'
 import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
@@ -29,3 +28,17 @@ export default function App({ Component, pageProps }) {
     </ThemeProvider>
   )
 }
+;<script
+  dangerouslySetInnerHTML={{
+    __html: `
+         (function(c,l,a,r,i,t,y){
+             c[a] = c[a] || function () { (c[a].q = c[a].q || 
+             []).push(arguments) };
+             t=l.createElement(r);
+             t.async=1;
+             t.src="https://www.clarity.ms/tag/"+i;
+             y=l.getElementsByTagName(r)[0];
+             y.parentNode.insertBefore(t,y);
+         })(window, document, "clarity", "script", "eiw0lj3ita");`,
+  }}
+/>
