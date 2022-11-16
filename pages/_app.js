d3890aa2428824ec43ab04d47f6fbcb8b6880a8f
Nicolas Kheirallah <Nicolas.kheirallah@gmail.com>
2022-11-16 10:16:45 +0100

    SharePoint

--- pages/_app.js
+++ pages/_app.js
@@ -14,28 +14,6 @@ import { ClientReload } from '@/components/ClientReload'
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
 
-function Page() {
-  return (
-    <>
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-         (function(c,l,a,r,i,t,y){
-             c[a] = c[a] || function () { (c[a].q = c[a].q || 
-             []).push(arguments) };
-             t=l.createElement(r);
-             t.async=1;
-             t.src="https://www.clarity.ms/tag/"+i;
-             y=l.getElementsByTagName(r)[0];
-             y.parentNode.insertBefore(t,y);
-         })(window, document, "clarity", "script", "eiw0lj3ita");`,
-        }}
-      />
-      ;
-    </>
-  )
-}
-
 export default function App({ Component, pageProps }) {
   this.Page()
   return (
